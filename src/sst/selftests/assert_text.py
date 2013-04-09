5fe963e496760961966518c2dbb4e679ba0b6606
Leo Arias <leo.arias@canonical.com>
2013-04-09 10:11:36 -0600

    Updated the assert text test.

--- src/sst/selftests/assert_text.py
+++ src/sst/selftests/assert_text.py
@@ -14,22 +14,11 @@ assert_text('some_id', 'Some text here')
 # Test wrong text.
 fails(assert_text, 'some_id', 'Wrong text')
 
-# Test no text with id.
+# Test no text.
 try:
     assert_text('element_without_text', '')
 except AssertionError as error:
     assert_equal(
-        "Element 'element_without_text' has no text attribute", error.message)
-else:
-    raise AssertionError('assert_text did not fail.')
-
-# Test no text with element.
-try:
-    assert_text(get_element(id='element_without_text'), '')
-except AssertionError as error:
-    expected_message = ("Element u\'<p id=\"element_without_text\"></p>\' "
-                        "has no text attribute")
-    assert_equal(
-        expected_message, error.message)
+        "Element u'element_without_text' has no text attribute", error.message)
 else:
     raise AssertionError('assert_text did not fail.')
