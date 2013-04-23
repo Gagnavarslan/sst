b893dddb2ca0046ab45021a9ef6878093753b818
Vincent Ladeuil <vila+qa@canonical.com>
2013-04-23 19:03:20 +0200

    Last remaining flake8 warning killed. TestLoader.loadTestsFromName issue diagnosed, not really a bug but a rather surprising behavior (too much magic kill the magic ;).

--- src/sst/tests/test_selftest.py
+++ src/sst/tests/test_selftest.py
@@ -22,28 +22,28 @@ from cStringIO import StringIO
 
 import testtools
 
-# The following two imports are required by unittest when running a list of
-# test names like test_list_single does below (that's probably a bug in
-# TestLoader.loadTestsFromName...) -- vila 2012-11-29
-import sst  # NOQA, see above
-import sst.tests  # NOQA, see above
 from sst.tests import main
 
 
 class TestTestProgram(testtools.TestCase):
 
-    def run_test_program(self, argv, out):
-        main.TestProgram(__name__, argv=['dummy'] + argv, stdout=out,
+    def run_test_program(self, module, argv, out):
+        main.TestProgram(module, argv=['dummy'] + argv, stdout=out,
                          exit=False)
 
     def test_list(self):
         out = StringIO()
-        self.run_test_program(['-l'], out)
+        # We need to specify __name__ as 'module' to get all the tests defined
+        # in this file
+        self.run_test_program(__name__, ['-l'], out)
         # At least this test should appear in the list (we don't check for an
         # exact list or this test will fail each time a new one is added).
         self.assertIn(self.id(), out.getvalue())
 
     def test_list_single(self):
         out = StringIO()
-        self.run_test_program(['-l', self.id()], out)
+        # We need to specify None as 'module' or
+        # unittest.loader.TestLoader.loadTestsFromName will not automatically
+        # import the required modules
+        self.run_test_program(None, ['-l', self.id()], out)
         self.assertEqual(self.id() + '\n', out.getvalue())
