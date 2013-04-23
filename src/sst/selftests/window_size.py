fd8824ba8adbfcc97a97685122a6416f138cf42f
Corey Goldberg <cgoldberg@gmail.com>
2013-04-23 16:45:12 -0400

    refactored window_size test

--- src/sst/selftests/window_size.py
+++ src/sst/selftests/window_size.py
@@ -7,24 +7,33 @@ assert isinstance(height, int)
 assert width > 0
 assert height > 0
 
-expected_width, expected_height = sst.actions.set_window_size(200, 300)
-width, height = sst.actions.get_window_size()
-assert isinstance(expected_width, int)
-assert isinstance(expected_height, int)
-assert width == expected_width == 200
-assert height == expected_height == 300
+width, height = sst.actions.set_window_size(200, 300)
+assert isinstance(width, int)
+assert isinstance(height, int)
+sst.actions.assert_equal(width, 200)
+sst.actions.assert_equal(height, 300)
 
-expected_width, expected_height = sst.actions.set_window_size(450, 420)
 width, height = sst.actions.get_window_size()
-assert width == expected_width == 450
-assert height == expected_height == 420
+assert isinstance(width, int)
+assert isinstance(height, int)
+sst.actions.assert_equal(width, 200)
+sst.actions.assert_equal(height, 300)
 
-expected_width, expected_height = sst.actions.set_window_size(380, 320)
+width, height = sst.actions.set_window_size(450, 420)
+sst.actions.assert_equal(width, 450)
+sst.actions.assert_equal(height, 420)
+
+width, height = sst.actions.get_window_size()
+sst.actions.assert_equal(width, 450)
+sst.actions.assert_equal(height, 420)
+
+width, height = sst.actions.set_window_size(380, 320)
 # set window to same size
 sst.actions.set_window_size(380, 320)
+
 width, height = sst.actions.get_window_size()
-assert width == expected_width == 380
-assert height == expected_height == 320
+sst.actions.assert_equal(width, 380)
+sst.actions.assert_equal(height, 320)
 
 sst.actions.go_to('/')
 
@@ -32,7 +41,11 @@ sst.actions.go_to('/')
 sst.actions.click_link('popup_link')
 sst.actions.switch_to_window(index_or_name='_NEW_WINDOW')
 sst.actions.assert_title('Popup Window')
-expected_width, expected_height = sst.actions.set_window_size(260, 275)
+
+width, height = sst.actions.set_window_size(260, 275)
+sst.actions.assert_equal(width, 260)
+sst.actions.assert_equal(height, 275)
+
 width, height = sst.actions.get_window_size()
-assert width == expected_width == 260
-assert height == expected_height == 275
+sst.actions.assert_equal(width, 260)
+sst.actions.assert_equal(height, 275)
