08cd73b9aec2a27bfc03364ef8b377d63448f2c3
Corey Goldberg <cgoldberg@gmail.com>
2013-05-22 09:32:36 -0400

    worker threaded concurrency tests

--- src/sst/tests/test_concurrency.py
+++ src/sst/tests/test_concurrency.py
@@ -7,13 +7,16 @@
 #  $ nosetests -v -m test_concurrent*
 #
 
+import os
+import Queue
 import sys
 import time
+import threading
 import unittest
 from cStringIO import StringIO
 
 import junitxml
-
+from selenium import webdriver
 from testtools import (
     ConcurrentTestSuite,
     iterate_tests,
@@ -32,15 +35,18 @@ def _make_test_suite(num_tests):
 
     # every method generated will have this body
     def test_method(self):
-        time.sleep(0.5)
+        driver = webdriver.Firefox()
+        driver.quit()
         self.assertTrue(True)
 
     # create a dict of test methods, sequentially named
     d = {}
     for i in range(num_tests):
         d['test_method%i' % i] = test_method
+
     # generate a Class with the created methods
     SampleTestCase = type('SampleTestCase', (TestCase,), d)
+
     # make a sequence of test_cases from the test methods
     test_cases = [
         SampleTestCase('test_method%i' % i)
@@ -55,7 +61,7 @@ class ConcurrencyTestCase(TestCase):
         super(ConcurrencyTestCase, self).setUp()
         sst.tests.set_cwd_to_tmp(self)
         self.addCleanup(self.restore_stdout)
-    
+
     def restore_stdout(self):
         sys.stdout = sys.__stdout__
 
@@ -69,7 +75,7 @@ class ConcurrencyTestCase(TestCase):
         return suite_groups
 
     def test_concurrent_all_at_once_text_result(self):
-        num_tests = 50
+        num_tests = 4
         original_suite = _make_test_suite(num_tests)
         out = StringIO()
         result = sst.result.TextTestResult(out, verbosity=0)
@@ -80,7 +86,7 @@ class ConcurrencyTestCase(TestCase):
         self.assertEqual(result.testsRun, num_tests)
 
     def test_concurrent_all_at_once_xml_result(self):
-        num_tests = 50
+        num_tests = 4
         original_suite = _make_test_suite(num_tests)
         out = StringIO()
         result = junitxml.JUnitXmlResult(out)
@@ -95,7 +101,7 @@ class ConcurrencyTestCase(TestCase):
         self.assertEqual(len(xml_lines), num_tests + 2)
 
     def test_concurrent_all_at_once_multi_result(self):
-        num_tests = 50
+        num_tests = 4
         original_suite = _make_test_suite(num_tests)
         txt_result = sst.result.TextTestResult(sys.stdout, verbosity=0)
         out = StringIO()
@@ -112,8 +118,8 @@ class ConcurrencyTestCase(TestCase):
         self.assertEqual(len(xml_lines), num_tests + 2)
 
     def test_concurrent_even_groups_text_result(self):
-        num_tests = 50
-        group_size = 25  # number of tests in each sub_suite
+        num_tests = 4
+        group_size = 2  # number of tests in each sub_suite
         original_suite = _make_test_suite(num_tests)
         sub_suites = self.group_cases(
             self.split_suite(original_suite),
@@ -128,9 +134,9 @@ class ConcurrencyTestCase(TestCase):
         self.assertEqual(result.testsRun, num_tests)
 
     def test_concurrent_even_groups_multi_result(self):
-        num_tests = 50
-        group_size = 25  # number of tests in each sub_suite
-        
+        num_tests = 4
+        group_size = 2  # number of tests in each sub_suite
+
         original_suite = _make_test_suite(num_tests)
         sub_suites = self.group_cases(
             self.split_suite(original_suite),
@@ -147,6 +153,60 @@ class ConcurrencyTestCase(TestCase):
         self.assertTrue(result.wasSuccessful())
         self.assertEqual(result.errors, [])
         self.assertEqual(result.testsRun, num_tests)
-        xml_lines = out.getvalue().splitlines() 
+        xml_lines = out.getvalue().splitlines()
+        # xml file has a line for each case + header + footer
+        self.assertEqual(len(xml_lines), num_tests + 2)
+
+    def test_concurrent_one_case_per_threaded_worker(self):
+        num_tests = 16
+        num_workers = 4  # number of worker threads to spawn
+
+        def worker(input):
+            # Get from input queue until empty
+            while True:
+                try:
+                    single_case_suite, result = input.get(False)
+                    # Run the test
+                    single_case_suite.run(result)
+                except Queue.Empty:
+                    break
+
+        # Genereate a regular unittest suite to run
+        original_suite = _make_test_suite(num_tests)
+
+        # Create results and streams.
+        txt_result = sst.result.TextTestResult(sys.stdout, verbosity=0)
+        out = StringIO()
+        xml_result = junitxml.JUnitXmlResult(out)
+        result = MultiTestResult(txt_result, xml_result)
+
+        # Split the suite into sub suites with one TestCase in each.
+        suites = self.split_suite(original_suite)
+
+        # Create queue for test suites
+        task_queue = Queue.Queue()
+
+        # Queue up tasks
+        for single_case_suite in suites:
+            task_queue.put((single_case_suite, result))
+
+        # Start workers
+        threads = []
+        for _ in xrange(num_workers):
+            t = threading.Thread(target=worker, args=(task_queue,))
+            t.start()
+            threads.append(t)
+
+        # Wait on workers
+        for t in threads:
+            t.join()
+
+        # Stop populating XML results.
+        result.stopTestRun()
+
+        self.assertTrue(result.wasSuccessful())
+        self.assertEqual(result.errors, [])
+        self.assertEqual(result.testsRun, num_tests)
+        xml_lines = out.getvalue().splitlines()
         # xml file has a line for each case + header + footer
         self.assertEqual(len(xml_lines), num_tests + 2)
