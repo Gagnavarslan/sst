c5b0fcd8e95e55ba91903861a71e5eed6adedacb
Corey Goldberg <cgoldberg@gmail.com>
2013-06-01 17:14:09 -0400

    moar unit tests for concurrency

--- src/sst/tests/test_concurrency.py
+++ src/sst/tests/test_concurrency.py
@@ -18,12 +18,13 @@ from testtools import (
 from sst import (
     concurrency,
     result,
+    runtests,
     tests,
 )
 
 
-def _make_test_suite(num_tests):
-    """ generate a TestSuite with some number of identical TestCases.
+def _make_allpass_test_suite(num_tests):
+    """ Generate a TestSuite with a number of identical TestCases.
 
     This is used for generating test data."""
 
@@ -47,10 +48,10 @@ def _make_test_suite(num_tests):
     return unittest.TestSuite(test_cases)
 
 
-class ConcurrencyTestCase(TestCase):
+class ConcurrencyForkedTestCase(TestCase):
 
     def setUp(self):
-        super(ConcurrencyTestCase, self).setUp()
+        super(ConcurrencyForkedTestCase, self).setUp()
         tests.set_cwd_to_tmp(self)
 
     def test_concurrent_forked(self):
@@ -63,7 +64,7 @@ class ConcurrencyTestCase(TestCase):
         xml_result = JUnitXmlResult(xml_out)
         res = MultiTestResult(txt_result, xml_result)
 
-        original_suite = _make_test_suite(num_tests)
+        original_suite = _make_allpass_test_suite(num_tests)
         suite = ConcurrentTestSuite(
             original_suite,
             lambda suite: concurrency.fork_for_tests(suite, concurrency_num)
@@ -80,3 +81,79 @@ class ConcurrencyTestCase(TestCase):
         xml_lines = xml_out.getvalue().splitlines()
         # xml file has a line for each case + header + footer
         self.assertEqual(len(xml_lines), num_tests + 2)
+
+
+class ConcurrencyRunTestCase(tests.ImportingLocalFilesTest):
+
+    def setUp(self):
+        super(ConcurrencyRunTestCase, self).setUp()
+
+    def test_runtests_concurrent_allpass(self):
+        tests.write_tree_from_desc('''dir: t
+file: t/__init__.py
+from sst import loader
+discover = loader.discoverRegularTests
+
+file: t/test_conc1.py
+import unittest
+class Test1(unittest.TestCase):
+    def test_me_1(self):
+        self.assertTrue(True)
+
+file: t/test_conc2.py
+import unittest
+class Test2(unittest.TestCase):
+    def test_me_2(self):
+        self.assertTrue(True)
+''')
+
+        self.out = StringIO()
+        self.patch(sys, 'stdout', self.out)
+
+        runtests.runtests(
+            ['^t'], 'no results directory used',
+            concurrency_num=2
+        )
+
+        output = self.out.getvalue()
+        lines = output.splitlines()
+        self.assertEqual('', lines[0])
+        self.assertIn('test cases loaded', output)
+        self.assertIn('Ran 2 tests', output)
+        self.assertIn('OK', output)
+        self.assertNotIn('FAIL', output)
+
+    def test_runtests_concurrent_onefail(self):
+        tests.write_tree_from_desc('''dir: t
+file: t/__init__.py
+from sst import loader
+discover = loader.discoverRegularTests
+
+file: t/test_pass.py
+import unittest
+class TestPass(unittest.TestCase):
+    def test_me(self):
+        self.assertTrue(True)
+
+file: t/test_fail.py
+import unittest
+class TestFail(unittest.TestCase):
+    def test_fail(self):
+        self.assertTrue(False)
+''')
+
+        self.out = StringIO()
+        self.patch(sys, 'stdout', self.out)
+
+        runtests.runtests(
+            ['^t'], 'no results directory used',
+            concurrency_num=2
+        )
+
+        output = self.out.getvalue()
+        lines = output.splitlines()
+        self.assertEqual('', lines[0])
+        self.assertIn('test cases loaded', output)
+        self.assertIn('Ran 2 tests', output)
+        self.assertIn('OK', output)
+        self.assertIn('FAIL', output)
