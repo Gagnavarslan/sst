235a83e501f4e8e7cbe8bb61d1c038e210882a40
Corey Goldberg <corey.goldberg@canonical.com>
2013-09-15 12:17:13 -0400

    fix parens

--- src/sst/command.py
+++ src/sst/command.py
@@ -157,7 +157,7 @@ def get_opts(get_options, args=None):
 
     if cmd_opts.browser_type not in browsers.browser_factories:
         print('Error: %s should be one of %s' %
-             (cmd_opts.browser_type, browsers.browser_factories.keys()))
+              cmd_opts.browser_type, browsers.browser_factories.keys())
         sys.exit(1)
 
     logging.basicConfig(format='    %(levelname)s:%(name)s:%(message)s')
