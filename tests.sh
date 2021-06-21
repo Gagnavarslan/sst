8c8db1c934b7e2162346f20f9624704b59362422
Roman Konoval <rkonoval@gmail.com>
2021-06-21 14:33:07 +0200

    adds option to run integration tests

--- tests.sh
+++ tests.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+if [ "$TOXBUILD" = "true" ]; then
+  exit 0
+fi
+
 echo $(pwd)
 export PYTHONPATH=$(pwd)/src
 # print version and exit
 sst-run -V
-# run sst unit tests
-sst-test -r xml -s -x --extended-tracebacks -e ^sst.tests.test_django_devserver -e ^sst.tests.test_code_format -e ^sst.tests.test_sst_script_test_case -e ^sst.selftests -e ^sst.tests.test_results
+
+if [ "$EXECUTE_INTEGRATION" != "" ]; then
+  sst-test -r xml -s -x --extended-tracebacks
+else
+  # run sst unit tests
+  sst-test -r xml -s -x --extended-tracebacks -e ^sst.tests.test_django_devserver -e ^sst.tests.test_code_format -e ^sst.tests.test_sst_script_test_case -e ^sst.selftests -e ^sst.tests.test_results
+fi
